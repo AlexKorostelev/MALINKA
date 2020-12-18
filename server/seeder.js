715c7c37b285b5a45d48b19ae563efeb88cd738c
Dainyus <fol-dem@mail.ru>
2020-12-18 11:19:37 +0300

    seeder finish

--- server/seeder.js
+++ server/seeder.js
@@ -5,6 +5,7 @@ const User = require('./models/user')
 const Home = require('./models/home')
 const PinSettings = require('./models/pinSettings')
 const UserTg = require('./models/userTg')
+const sha256 = require('sha256')
 
 // lorem settings
 const lorem = new LoremIpsum({
@@ -28,13 +29,6 @@ mongoose.connect(process.env.MONGOOSE_CONNECT, {
   console.log('DB connected');
 });
 
-// const testUser = new User({
-//   name: 'testName',
-//   password: '123',
-//   email: 'testMail@mail.com',
-//   homes: 
-// })
-
 const testSetting = new PinSettings({
   name: lorem.generateWords(1),
   pinNum: Math.floor(Math.random() * 40),
@@ -53,13 +47,51 @@ const testSetting2 = new PinSettings({
   pinType: 'input'
 })
 
+async function seedHome() {
+  const dbArrPinSettings = await PinSettings.find()
+  const ArIdrPinSettings = dbArrPinSettings.map(el => el._id)
 
-async function seed(model, arr) {
+  const home = new Home({
+    name: lorem.generateWords(1),
+    location: lorem.generateWords(2) + Math.floor(Math.random() * 10),
+    pinSettingsId: [ArIdrPinSettings[0]]
+  })
 
-  await model.insertMany(arr);
+  const home1 = new Home({
+    name: lorem.generateWords(1),
+    location: lorem.generateWords(2) + Math.floor(Math.random() * 10),
+    pinSettingsId: [ArIdrPinSettings[1], ArIdrPinSettings[2]]
+  })
 
+  await Home.insertMany([home, home1]);
   await mongoose.disconnect();
   console.log('DB disconnected');
 }
 
-seed(PinSettings, [testSetting, testSetting1, testSetting2])
+async function seed(model, arr) {
+  await model.insertMany(arr);
+  await mongoose.disconnect();
+  console.log('DB disconnected');
+}
+
+async function seedUser() {
+  const dbArrHomes = await Home.find()
+  console.log(dbArrHomes);
+  const ArrIdHomes = dbArrHomes.map(el => el._id)
+  console.log(ArrIdHomes);
+
+  const testUser = new User({
+    name: lorem.generateWords(1),
+    password: sha256('123'),
+    email: lorem.generateWords(1),
+    homes: ArrIdHomes,
+  })
+
+  await testUser.save()
+  await mongoose.disconnect();
+  console.log('DB disconnected');
+}
+
+// seedUser()
+// seedHome()
+// seed(PinSettings, [testSetting, testSetting1, testSetting2])
