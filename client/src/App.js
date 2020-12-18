4b7c39d1a104b79ecf1fb2857d07a67dd64c634a
Dainyus <fol-dem@mail.ru>
2020-12-18 12:25:52 +0300

    prepare routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,29 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Header from './Header/Header'
+import Main from './Main/Main'
+import Cabinet from './Cabinet/Cabinet'
 
 function App() {
   return (
-    <>
-      <h1>Main</h1>
-    </>
+    <Router>
+      <Header />
+
+      <Switch>
+
+        <Route exact path='/'>
+          <Main />
+        </Route>
+
+        <Route path='/cabinet'>
+          <Cabinet />
+        </Route>
+
+      </Switch>
+    </Router>
   );
 }
 
---
+++ client/src/Cabinet/Cabinet.js
@@ -0,0 +1,5 @@
+export default function Cabinet() {
+  return (
+    <h1>Cabinet</h1>
+  )
+}
