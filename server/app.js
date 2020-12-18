7faa8c95c8569a2c749346680160bf3b662629b4
 Aleksei <aleksei@MacBook-Pro-Aleksei.local>
2020-12-18 16:02:20 +0300

    auth done

--- server/app.js
+++ server/app.js
@@ -1,14 +1,17 @@
 require('dotenv').config()
 const express = require('express');
-const session = require('express-session');
-const MongoStore = require('connect-mongodb-session')(session);
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 const cors = require('cors');
 const authRoutes = require('./routes/auth');
+const userMiddeleware = require('./middelware/user');
+const varMiddelware = require('./middelware/variables');
+
 const app = express()
 
 const indexRouter = require('./routes/index')
-
+const url = process.env.MONGOOSE_CONNECT
 mongoose.connect(process.env.MONGOOSE_CONNECT, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -19,25 +22,33 @@ mongoose.connect(process.env.MONGOOSE_CONNECT, {
   console.log('DB connected');
 });
 
-const store = new MongoStore({
-  collection: 'sessions',
-  url: process.env.MONGOOSE_CONNECT,
-})
 
-app.use(session({
-  secret: 'srtwyrwtywrtywrtyrtwy',
-  resave: false,
-  saveUninitialized: false,
-  store,
-}))
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 
+app.use(session({
+  secret: 'fdlk',
+  resave: false,
+  saveUninitialized: false,
+  store: new MongoStore({
+    mongooseConnection: mongoose.createConnection(process.env.MONGOOSE_CONNECT, {useNewUrlParser: true,  useUnifiedTopology: true,}),
+
+    // collection: 'sessions',
+    // url,
+  })
+}))
+
+
+app.use(userMiddeleware);
+app.use(varMiddelware);
+
 // routes
 app.use('/', indexRouter)
 app.use('/auth', authRoutes)
+
 
 
 const PORT = process.env.PORT ?? 3001
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,24 +1,50 @@
 const { Router } = require('express');
 const User = require('../models/user');
+const sha256 = require('sha256')
 
 const router = Router();
 
 router.post('/login', async (req, res) => {
-
 	const {name, password, email}  = req.body;
 
 	const candidate = await User.findOne({email})
 
-	if(password === candidate.password) {
+	if(sha256(password) === candidate.password) {
 		req.session.user = {email};
 		req.session.isAuthenticated = true;
 		req.session.save((err) => {
 			if(err) {
 				console.log(err);
 			}
-			return res.sendStatus(200)
 		})
+		return res.sendStatus(200)
 	}
+})
+
+router.post('/register', async (req, res) => {
+  try {
+    const {
+      email, password, homes, name, tgLogin
+    } = req.body;
+    const candidate = await User.findOne({ email });
+
+    if (candidate) {
+			res.sendStatus(400);	
+    } else {
+      const user = new User({
+        email, password: sha256(password), homes, name, tgLogin
+			}).save();
+      res.json(user);
+    }
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+router.get('/logout', async (req, res) => {
+	req.session.destroy(() => {
+		res.sendStatus(200);
+	});
 })
 
 module.exports = router;
