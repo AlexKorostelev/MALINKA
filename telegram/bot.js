d3b1a19a654ab6b3ca5a32185cec678f93554da5
Stanley <stanislav.v.grishin@gmail.com>
2020-12-18 19:59:57 +0300

    Unique Files creation added

--- telegram/bot.js
+++ telegram/bot.js
@@ -11,115 +11,106 @@ const folderId = process.env.FOLDER_ID;
 const yandexToken = process.env.YANDEX_TOKEN;
 const mongoDB = process.env.MONGOOSE_DB;
 const bot = new Telegraf(telegramBotToken);
-// const ContextMessageUpdate = require('telegraf');
 
-mongoose.connect(mongoDB, { useNewUrlParser: true });
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const connection = mongoose.connection;
 connection.once('open', async () => {
   console.log('MongoDB database connection established successfully');
 });
 
-const checkIfRegistered = async (checkUserTelegramName) => {
-  const userFound = await User.findOne({ tgLogin: checkUserTelegramName });
-  return userFound
-};
-
-let userTelegramName = '';
-let notRegisteredName = '';
-bot.start(async (ctx) => {
-  userTelegramName = ctx.message.from.username;
-  notRegisteredName = ctx.message.from.username;
-  const isThereUser = await User.findOne({ tgLogin: userTelegramName });
-  if (isThereUser) {
-    ctx.reply(`Greetings, ${userTelegramName}`);
+const checkIfRegistered = async (ctx, next) => {
+  const userFound = await User.findOne({ tgLogin: ctx.message.from.username });
+  if (userFound) {
+    await next();
   } else {
     ctx.reply(
-      `Dear, ${notRegisteredName}! You have not been registered by our managers`
+      `Dear, ${ctx.message.from.username}! You have not been registered by our managers`
     );
-    userTelegramName = '';
   }
+};
+
+const giveNameToFile = (userIdTg)=>{
+  let localDate = (new Date(Date.now())).toISOString().toString().slice(0,-5)
+
+  const dir = `./allTheSounds/${userIdTg}`;
+
+  if (!fs.existsSync(dir)){
+    fs.mkdirSync(dir);
+  }
+  return `./allTheSounds/${userIdTg}/${userIdTg}_${localDate}.ogg`
+}
+
+bot.use(checkIfRegistered);
+
+bot.start(async (ctx) => {
+  ctx.reply(`Greetings, ${ctx.message.from.username}`);
 });
 
 bot.help(async (ctx) => {
-  ctx.reply(`I am ready to give you a hand, ${notRegisteredName}`);
+  ctx.reply(`I am ready to give you a hand, ${ctx.message.from.username}`);
 });
 bot.command('myHomes', async (ctx) => {
-  if (userTelegramName) {
-    let userHomes = await User.findOne({ tgLogin: userTelegramName }).populate(
-      'homes'
-    );
-    let userHomesToReturn = userHomes.homes.map(
-      (eachHome) => `${eachHome.name}`
-    );
-    ctx.reply(
-      `Here is the list of your homes:\n${userHomesToReturn.join('\n')}`
-    );
-  } else {
-    ctx.reply(
-      `You can't use the service unless you are registered in IoT Russia`
-    );
-  }
+  let userHomes = await User.findOne({
+    tgLogin: ctx.update.message.from.username,
+  }).populate('homes');
+  let userHomesToReturn = userHomes.homes.map((eachHome) => `${eachHome.name}`);
+  console.log(ctx.update.message);
+  ctx.reply(`Here is the list of your homes:\n${userHomesToReturn.join('\n')}`);
 });
 bot.on('message', async (ctx) => {
-  if (!userTelegramName) {
-    ctx.reply(
-      `You can't use the service unless you are register in IoT Russia`
+  if (!ctx.update.message.voice) {
+    ctx.reply('Could you please send Audio message instead');
+    return;
+  }
+  try {
+    const fileID = ctx.update.message.voice.file_id;
+    const response = await fetch(
+      `https://api.telegram.org/bot${telegramBotToken}/getFile?file_id=${fileID}`
     );
-  } else {
-    if (!ctx.update.message.voice) {
-      ctx.reply('Could you please send Audio message instead');
-      return;
+    const rez = await response.json();
+    let nameOfTheFile = giveNameToFile(ctx.update.message.from.id)
+
+    const file = fs.createWriteStream(nameOfTheFile);
+
+    function sound2Text(fileName) {
+      let binary = fs.readFileSync(`./${fileName}`);
+
+      const options = {
+        method: 'POST',
+        url: `https://stt.api.cloud.yandex.net/speech/v1/stt:recognize?lang=ru-RU&topic=general&profanityFilter=false&format=oggopus&sampleRateHertz=16000&folderId=${folderId}`,
+        headers: {
+          'Content-Type': 'audio/ogg',
+          Authorization: `Bearer ${yandexToken}`,
+        },
+        body: binary,
+      };
+      request(options, (error, response) => {
+        if (error) throw new Error(error);
+        console.log(
+          '\x1b[1m\x1b[33m%s\x1b[0m',
+          JSON.parse(response.body).result
+        );
+      });
     }
-    try {
-      const fileID = ctx.update.message.voice.file_id;
-      const response = await fetch(
-        `https://api.telegram.org/bot${telegramBotToken}/getFile?file_id=${fileID}`
-      );
-      const rez = await response.json();
-      console.log(ctx.update.message);
-
-      const file = fs.createWriteStream('file.ogg');
-
-      function sound2Text(fileName) {
-        let binary = fs.readFileSync(`./${fileName}`);
-
-        const options = {
-          method: 'POST',
-          url: `https://stt.api.cloud.yandex.net/speech/v1/stt:recognize?lang=ru-RU&topic=general&profanityFilter=false&format=oggopus&sampleRateHertz=16000&folderId=${folderId}`,
-          headers: {
-            'Content-Type': 'audio/ogg',
-            Authorization: `Bearer ${yandexToken}`,
-          },
-          body: binary,
-        };
-        request(options, (error, response) => {
-          if (error) throw new Error(error);
-          console.log(
-            '\x1b[1m\x1b[33m%s\x1b[0m',
-            JSON.parse(response.body).result
-          );
-        });
-      }
-      fetch(
-        `https://api.telegram.org/file/bot${telegramBotToken}/${rez.result.file_path}`
-      )
-        .then(
-          (response) =>
-            new Promise((resolve, reject) => {
-              response.body.pipe(file);
-              file.on('finish', () => {
-                setTimeout(() => {
-                  resolve();
-                });
+    fetch(
+      `https://api.telegram.org/file/bot${telegramBotToken}/${rez.result.file_path}`
+    )
+      .then(
+        (response) =>
+          new Promise((resolve, reject) => {
+            response.body.pipe(file);
+            file.on('finish', () => {
+              setTimeout(() => {
+                resolve();
               });
-            })
-        )
-        .then(() => {
-          sound2Text('file.ogg');
-        });
-    } catch (error) {
-      console.log('Unfortunately, we have got an error, milord\n', error);
-    }
+            });
+          })
+      )
+      .then(() => {
+        sound2Text(nameOfTheFile);
+      });
+  } catch (error) {
+    console.log('Unfortunately, we have got an error, milord\n', error);
   }
 });
 bot.launch();
