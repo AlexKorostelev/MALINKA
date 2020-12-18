b453e1a68cb6eef8e380b71b51b185e91c9d174b
Stanley <stanislav.v.grishin@gmail.com>
2020-12-18 11:33:48 +0300

    Error handling added

--- telegram/bot.js
+++ telegram/bot.js
@@ -12,48 +12,59 @@ bot.start((ctx) => ctx.reply(`Greetings, ${ctx.message.from.username}`));
 bot.help((ctx) => ctx.reply('Send me a sticker'));
 
 bot.on('message', async (ctx) => {
-  const fileID = ctx.update.message.voice.file_id;
-  const response = await fetch(
-    `https://api.telegram.org/bot${telegramBotToken}/getFile?file_id=${fileID}`
-  );
-  const rez = await response.json();
-  console.log(ctx.update.message);
-
-  const file = fs.createWriteStream('file.ogg');
-
-  function sound2Text(fileName) {
-    let binary = fs.readFileSync(`./${fileName}`);
-
-    const options = {
-      method: 'POST',
-      url: `https://stt.api.cloud.yandex.net/speech/v1/stt:recognize?lang=ru-RU&topic=general&profanityFilter=false&format=oggopus&sampleRateHertz=16000&folderId=${folderId}`,
-      headers: {
-        'Content-Type': 'audio/ogg',
-        Authorization: `Bearer ${yandexToken}`,
-      },
-      body: binary,
-    };
-    request(options, (error, response) => {
-      if (error) throw new Error(error);
-      console.log('\x1b[1m\x1b[33m%s\x1b[0m', JSON.parse(response.body).result);
-    });
+  if (!ctx.update.message.voice) {
+    ctx.reply('Could you please send Audio message instead');
+    return;
   }
-  fetch(
-    `https://api.telegram.org/file/bot${telegramBotToken}/${rez.result.file_path}`
-  )
-    .then(
-      (response) =>
-        new Promise((resolve, reject) => {
-          response.body.pipe(file);
-          file.on('finish', () => {
-            setTimeout(() => {
-              resolve();
-            });
-          });
-        })
+  try {
+    const fileID = ctx.update.message.voice.file_id;
+    const response = await fetch(
+      `https://api.telegram.org/bot${telegramBotToken}/getFile?file_id=${fileID}`
+    );
+    const rez = await response.json();
+    console.log(ctx.update.message);
+
+    const file = fs.createWriteStream('file.ogg');
+
+    function sound2Text(fileName) {
+      let binary = fs.readFileSync(`./${fileName}`);
+
+      const options = {
+        method: 'POST',
+        url: `https://stt.api.cloud.yandex.net/speech/v1/stt:recognize?lang=ru-RU&topic=general&profanityFilter=false&format=oggopus&sampleRateHertz=16000&folderId=${folderId}`,
+        headers: {
+          'Content-Type': 'audio/ogg',
+          Authorization: `Bearer ${yandexToken}`,
+        },
+        body: binary,
+      };
+      request(options, (error, response) => {
+        if (error) throw new Error(error);
+        console.log(
+          '\x1b[1m\x1b[33m%s\x1b[0m',
+          JSON.parse(response.body).result
+        );
+      });
+    }
+    fetch(
+      `https://api.telegram.org/file/bot${telegramBotToken}/${rez.result.file_path}`
     )
-    .then(() => {
-      sound2Text('file.ogg');
-    });
+      .then(
+        (response) =>
+          new Promise((resolve, reject) => {
+            response.body.pipe(file);
+            file.on('finish', () => {
+              setTimeout(() => {
+                resolve();
+              });
+            });
+          })
+      )
+      .then(() => {
+        sound2Text('file.ogg');
+      });
+  } catch (error) {
+    console.log('Unfortunately, we have got an error, milord\n', error);
+  }
 });
 bot.launch();
