2fa227a823d53ce03f7763f65aa0d63712c50d4c
Dainyus <fol-dem@mail.ru>
2020-12-19 15:03:14 +0300

    pre commit

--- client/src/Cabinet/Cabinet.js
+++ client/src/Cabinet/Cabinet.js
@@ -1,5 +1,41 @@
+import React from 'react';
+import './style.css'
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import SwitchesGroup from './MaterialComponents/Light/index'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(3),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
 export default function Cabinet() {
+  const classes = useStyles();
   return (
-    <h1>Cabinet</h1>
-  )
+    <div className={classes.root}>
+      <h1>Control Panel</h1>
+      <Grid container spacing={1}>
+
+        <Grid item xs={6}>
+          <Paper className={`${classes.paper} blocks`}>
+            <SwitchesGroup />
+
+          </Paper>
+          
+        </Grid>
+
+        <Grid item xs={6}>
+          <Paper className={`${classes.paper} blocks`}>Датчики</Paper>
+        </Grid>
+
+      </Grid>
+    </div>
+  );
 }
