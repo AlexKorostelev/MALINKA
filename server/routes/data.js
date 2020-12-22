ed9d74f74a2bce6740a3579ec3fc58fdc0ddde10
 Aleksei <aleksei@MacBook-Pro-Aleksei.local>
2020-12-22 16:01:52 +0300

    add sockets

--- server/routes/data.js
+++ server/routes/data.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const mydata = require('../app')
 const PinSettings = require('../models/pinSettings')
 
-router.get('/', async (req, res) => {
-  const pinSettings = await PinSettings.find({}).populate()
-  console.log(pinSettings);
-  res.json(pinSettings)
-})
+// router.get('/', async (req, res) => {
+//   // const pinSettings = await PinSettings.find({}).populate()
+//   console.log(mydata);
+//   res.json(mydata);
+// })
 
 module.exports = router;
